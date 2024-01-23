7b848af582d06634826d0eb00af7f16433dab910
tobiassilalahi <tobias.silalahi@gmail.com>
2024-01-23 18:13:13 +1100

    debug by adding print to views and adding DUAR to the posts loop on HTML

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,5 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #posts = Post.objects.all()
+    print(posts)
     return render(request, 'blog/post_list.html', {'posts':posts})
